94c83037e9f302ee47ce625695a7647e67b3c5f5
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-12 11:27:19 +0200

    Dodaj pliki projektów.

---
+++ 5M02Slider/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace _5M02Slider
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 5M02Slider/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace _5M02Slider
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onSliderChanged(object sender, EventArgs e)
+        {
+            lblNapis.FontSize = slSlider.Value;
+            lblInfo.Text = $"rozmiar czcionki: {slSlider.Value}";
+        }
+        private void onBtnClicked(object sender, EventArgs e)
+        {
+            slSlider.Value = 30;
+        }
+    }
+
+}
